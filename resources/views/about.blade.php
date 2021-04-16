c7fc9ce56619162dcf496ed0c39e7972bdfb4ee8
nflorijan <florijan.nadj@gmail.com>
2021-04-16 18:29:14 +0200

    Creating about us page and route

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>About us</title>
+     <!-- Fonts -->
+     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+     <style>
+         body {
+             font-family: 'Nunito', sans-serif;
+         }
+      </style>
+  </head>
+  <body>
+    <h2>About us</h2>
+  </body>
+</html>
