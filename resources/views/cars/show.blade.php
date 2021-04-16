f75d245477b6087aa60a9af6e903e5c72a231659
nflorijan <florijan.nadj@gmail.com>
2021-04-16 19:36:51 +0200

    Add show all cars button

--- resources/views/cars/show.blade.php
+++ resources/views/cars/show.blade.php
@@ -3,6 +3,7 @@
 @section('title', $car->title)
 
 @section('content')
+  <a href="/cars"><< Show all cars</a>
   <h2>Car Title: {{ $car->title }}</h2>
   <p>Car Producer: {{ $car->producer }}</p>
   <p>Number of dors: {{ $car->number_of_dors }}</p>
